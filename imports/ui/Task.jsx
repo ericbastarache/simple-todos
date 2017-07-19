eeaa52bd65e214f8cbebea1544f9a1984b028f08
Eric Bastarache <eric_bastarache@hotmail.com>
2017-07-18 21:27:37 -0400

    Updated task component to allow for private/public todos

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
-
-import { Tasks } from '../api/tasks.js';
+import { Meteor } from 'meteor/meteor';
+import classnames from 'classnames';
 
 export default class Task extends Component {
 
@@ -12,8 +12,15 @@ export default class Task extends Component {
     Meteor.call('tasks.remove', this.props.task._id);
   }
 
+  togglePrivate = () => {
+    Meteor.call('tasks.setPrivate', this.props.task._id, !this.props.task.private);
+  }
+
   render() {
-    const taskClassName = this.props.task.checked ? 'checked' : '';
+    const taskClassName = classnames({
+      checked: this.props.task.checked,
+      private: this.props.task.private,
+    });
     return (
       <li className={taskClassName}>
         <button className="delete" onClick={this.deleteThisTask}>
@@ -27,6 +34,12 @@ export default class Task extends Component {
           onClick={this.toggleChecked}
           />
 
+        {this.props.showPrivateButton ? (
+          <button className="toggle-private" onClick={this.togglePrivate}>
+            {this.props.task.private ? 'Private' : 'Public'}
+          </button>
+        ) : ''}
+
         <span className="text">
           <strong>{this.props.task.username}</strong>:
           {this.props.task.text}
@@ -39,4 +52,5 @@ export default class Task extends Component {
 
 Task.propTypes = {
   task: PropTypes.object.isRequired,
+  showPrivateButton: React.PropTypes.bool.isRequired,
 };
