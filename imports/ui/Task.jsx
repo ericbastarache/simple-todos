dbf17bb1055ef7f4a197b0312b075f04cb38cb37
Eric Bastarache <eric_bastarache@hotmail.com>
2017-07-18 21:12:21 -0400

    Updated Task to use API methods

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -5,13 +5,11 @@ import { Tasks } from '../api/tasks.js';
 export default class Task extends Component {
 
   toggleChecked = () => {
-    Tasks.update(this.props.task._id, {
-      $set: { checked: !this.props.task.checked },
-    });
+    Meteor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked);
   }
 
   deleteThisTask = () => {
-    Tasks.remove(this.props.task._id);
+    Meteor.call('tasks.remove', this.props.task._id);
   }
 
   render() {
@@ -30,7 +28,7 @@ export default class Task extends Component {
           />
 
         <span className="text">
-          <strong>{this.props.task.username}</strong>: 
+          <strong>{this.props.task.username}</strong>:
           {this.props.task.text}
         </span>
       </li>
